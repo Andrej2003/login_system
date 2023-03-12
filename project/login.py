562315fd9c80952a48c89c3a3653189c887b09a0
Andrej2003 <andrej.nedelkovski.an@gmail.com>
2023-03-12 20:02:15 +0100

    render login & signup templates

--- project/login.py
+++ project/login.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from . import db
 
 log_in = Blueprint('login', __name__)
@@ -6,12 +6,12 @@ log_in = Blueprint('login', __name__)
 
 @log_in.route('/login')
 def login():
-    return 'Login'
+    return render_template("login.html")
 
 
 @log_in.route('/signup')
 def signup():
-    return 'Signup'
+    return render_template("signup.html")
 
 
 @log_in.route('/logout')
