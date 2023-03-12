e13640c91da7d7345f14e098c052b2cfc518e6ea
Andrej2003 <andrej.nedelkovski.an@gmail.com>
2023-03-12 20:29:02 +0100

    create signup_post

--- project/login.py
+++ project/login.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from . import db
 
 log_in = Blueprint('login', __name__)
@@ -12,6 +12,12 @@ def login():
 @log_in.route('/signup')
 def signup():
     return render_template("signup.html")
+
+
+@log_in.route('/signup', methods=['POST'])
+def signup_post():
+    # code to validate and add user to database goes here
+    return redirect(url_for('auth.login'))
 
 
 @log_in.route('/logout')
